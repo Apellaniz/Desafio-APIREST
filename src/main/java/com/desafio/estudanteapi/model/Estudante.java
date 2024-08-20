17d48b6f590ede47e9d120bc102a703b8a60fac7
Gabriel Apellaniz <gabriel.apellaniz@gmail.com>
2024-08-19 21:52:55 -0300

    Reconfigurando os getters e setters, para evitar erros.

--- src/main/java/com/desafio/estudanteapi/model/Estudante.java
+++ src/main/java/com/desafio/estudanteapi/model/Estudante.java
@@ -24,4 +24,44 @@ public class Estudante {
     private LocalDate dataNascimento;
 
     private String matricula;
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public LocalDate getDataNascimento() {
+        return dataNascimento;
+    }
+
+    public void setDataNascimento(LocalDate dataNascimento) {
+        this.dataNascimento = dataNascimento;
+    }
+
+    public String getMatricula() {
+        return matricula;
+    }
+
+    public void setMatricula(String matricula) {
+        this.matricula = matricula;
+    }
 }
